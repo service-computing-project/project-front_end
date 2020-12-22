1b83cecd2620b783a42dfbc151b29e1124c674c7
sherryjw <1024992493@qq.com>
2020-12-22 13:37:54 +0800

    Token-test

--- src/app/_service/auth.interceptor.ts
+++ src/app/_service/auth.interceptor.ts
@@ -1,28 +1,27 @@
 import { Observable } from 'rxjs';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    setTimeout(() => {
-      let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-      console.log(req);
-      console.log(currentUser);
-      if (currentUser && currentUser.Data) {
-        const newReq = req.clone({
-          withCredentials: true,
-          setHeaders: {
-            Authorization: `${currentUser.Data}`
-          }
-        });
-        console.log(newReq);
-        return next.handle(newReq);
-        // return next.handle(newReq).pipe(retry(2));
-      }
-    }, 500);
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(req);
+    console.log(currentUser);
+    if (currentUser && currentUser.Data) {
+      const newReq = req.clone({
+        withCredentials: true,
+        setHeaders: {
+          Authorization: `${currentUser.Data}`
+        }
+      });
+      console.log(newReq);
+      // return next.handle(newReq);
+      return next.handle(newReq).pipe(retry(2));
+    }
     return next.handle(req);
   }
 }
